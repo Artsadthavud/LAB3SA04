2cfbd3d15a9ac1aa8ef24ea626512cc28ba975f6
Artsadthavud <56318713+Artsadthavud@users.noreply.github.com>
2020-08-27 13:39:28 +0700

    add Forecast && BG

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Weather from './components/Weather';
+
+
 export default function App() {
  
  return (
--- components/Weather.js
+++ components/Weather.js
@@ -1,9 +1,29 @@
-import React from 'react';
-import { Text } from 'react-native';
-export default function Weather(props) {
-    return (
-    <Text>{props.zipCode}</Text>
-    );
+import React,{useState} from 'react';
+import Forecast from './Forecast'
+import { Text, StyleSheet,ImageBackground} from 'react-native';
 
-   }
-   
+
+export default function Weather(props) {
+    const [forecastInfo, setForecastInfo] = useState({
+    main: '-',
+    description: '-',
+    temp: 0
+    })
+    return (
+       
+        <ImageBackground source={require('../sxz.jpg')} style={styles.backdrop}>
+         <Text>Zip Code</Text>
+        <Text>{props.zipCode}</Text>
+        <Forecast {...forecastInfo}/>
+        </ImageBackground>
+      
+        )
+    }
+
+       const styles = StyleSheet.create({
+        backdrop: {
+        alignItems: 'center',
+        width: '100%',
+        height: '100%'
+        },
+       });
