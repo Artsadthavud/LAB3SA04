bb3a3326a48ee9a2988a5c3e530bc04c64f4f045
Artsadthavud <56318713+Artsadthavud@users.noreply.github.com>
2020-08-27 12:43:20 +0700

    add ZIP CODE

--- App.js
+++ App.js
@@ -3,9 +3,7 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Weather from './components/Weather';
 export default function App() {
- const doIt = () => {
- console.log("Hello from console")
- }
+ 
  return (
 
     <View style={styles.container}>
